a3563ffac01f382665d1694ff41f077b9026f896
AdiosSySni <adiossysni@gmail.com>
2022-10-03 12:21:41 +0200

    new app

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -4,49 +4,45 @@ namespace BindingLayout;
 
 public partial class MainPage : ContentPage
 {
-	//int count = 0;
-	VerticalStackLayout vs;
-	string text;
-	Label One;
+
 	public MainPage()
 	{
-		Color c = new Color();
-		c = Color.FromRgb(0, 100, 150);
-		Button btn = new Button();
-		btn.Clicked += OnCounterClicked;
-		btn.Clicked += AddNewPage;
-		Image img = new Image() { Source = "dotnet_bot.svg" };
-        text = "ThirdElem";
-        One = new Label();
+        InitializeComponent();
 
-		vs = new VerticalStackLayout()
-		{
-			img, new Label() {Text="FirstElem"}, new Label() {Text="SecondElem"}, btn
-        };
-		
-		vs.BackgroundColor = c;
-		this.Content = vs;
-		
     }
 
-	public void OnCounterClicked(object sender, EventArgs e)
+	//public async void FilePickerButton(object sender, EventArgs e)
+	//{
+ //       var result = await FilePicker.PickAsync(new PickOptions
+	//	{
+	//		PickerTitle = "Выберите изображение",
+	//		FileTypes = FilePickerFileType.Images
+            
+ //       });
+	//	if (result == null)
+	//		return;	
+
+	//	var stream = await result.OpenReadAsync();
+	//	myImage.Source = ImageSource.FromStream(() => stream);
+
+	//}
+
+    public void ClickButton(object sender, EventArgs e)
 	{
-		Label newLabel = new Label() { Text = $"{text}" };
-		(vs.Children[vs.Children.Count - 2] as Label).Text = "not lAST";
-		(vs.Children[vs.Children.Count - 3] as Label).Text = vs.Children.Count.ToString();
-        vs.Insert(vs.Children.Count-1, newLabel);
-		
-		//string s =$"<Label Text = \"{text}\"/>";
-		//One.LoadFromXaml(s);
+        //Person person = new Person()
+        //{
+        //    Name = NameEntry.Text,
+        //    Surname = SurnameEntry.Text,
+        //    ThirdName = ThirdNameEntry.Text 
+        //};
 
-	}
+        Preferences.Default.Set("Name", NameEntry.Text);
+        Preferences.Default.Set("Surname", SurnameEntry.Text);
+        Preferences.Default.Set("ThirdName", ThirdNameEntry.Text);
+        Preferences.Default.Set("URL", ImageURL.Text);
 
-	public async void AddNewPage(object sender, EventArgs e)
-	{
-		// await Shell.Current.GoToAsync("nwpg1");
-		await Shell.Current.GoToAsync("nwpg1?input=kyky");
-	}
+        Navigation.PushAsync(new NewPage1());
 
-	
+    }
 }
 
--- NewPage1.xaml.cs
+++ NewPage1.xaml.cs
@@ -5,10 +5,15 @@ public partial class NewPage1 : ContentPage
 	public NewPage1()
 	{
 		InitializeComponent();
-	}
+        Name.Text = Preferences.Default.Get("Name", "Unknown");
+        Surname.Text = Preferences.Default.Get("Surname", "Unknown");
+        ThirdName.Text = Preferences.Default.Get("ThirdName", "Unknown");
+        Image.Source = Preferences.Default.Get("URL", "Unknown");
+    }
 
-	private async void Button_Clicked(object sender, EventArgs e)
-	{
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+                
+    }
 
-	}
 }
